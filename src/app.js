70541d4c532ca8c1d7013afb1c27ffdb28384561
Jean Carlo Mata <jean.cms@hotmail.es>
2021-04-21 18:11:34 -0600

    init commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const app = new App();
+
+app.use(
+  new Alexa(),
+  new GoogleAssistant(),
+  new JovoDebugger(),
+  new FileDb()
+);
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+  LAUNCH() {
+    return this.toIntent('HelloWorldIntent');
+  },
+
+  HelloWorldIntent() {
+    this.ask("Hola Mundo amigo! Cuál es tu nombre?", 'Por favor dime tu nombre.');
+  },
+
+  MyNameIsIntent() {
+    this.tell('Oye ' + this.$inputs.name.value + ', un gusto conocerte!');
+  },
+});
+
+module.exports = { app };
